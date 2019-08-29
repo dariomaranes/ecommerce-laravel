5c5fe34b60a72f4f9513a5c177d8cc0afd18924b
Dario <dario.sjs@gmail.com>
2019-08-29 00:01:56 -0300

    Creo controlador para EntidadFaker

---
+++ app/Http/Controllers/EntidadFakerController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\entidadFaker;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EntidadFakerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\entidadFaker  $entidadFaker
+     * @return \Illuminate\Http\Response
+     */
+    public function show(entidadFaker $entidadFaker)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\entidadFaker  $entidadFaker
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(entidadFaker $entidadFaker)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\entidadFaker  $entidadFaker
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, entidadFaker $entidadFaker)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\entidadFaker  $entidadFaker
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(entidadFaker $entidadFaker)
+    {
+        //
+    }
+}
