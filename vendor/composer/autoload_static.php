c834a532445ab10963d5d44b9eb8593348fcfbb8
Dario <dario.sjs@gmail.com>
2019-08-29 00:01:13 -0300

    Composer update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -409,6 +409,7 @@ class ComposerStaticInitdc0e4323ea3289cdee4bb517efdc3bd0
         'App\\Providers\\EventServiceProvider' => __DIR__ . '/../..' . '/app/Providers/EventServiceProvider.php',
         'App\\Providers\\RouteServiceProvider' => __DIR__ . '/../..' . '/app/Providers/RouteServiceProvider.php',
         'App\\User' => __DIR__ . '/../..' . '/app/User.php',
+        'App\\entidadFaker' => __DIR__ . '/../..' . '/app/entidadFaker.php',
         'BeyondCode\\DumpServer\\DumpServerCommand' => __DIR__ . '/..' . '/beyondcode/laravel-dump-server/src/DumpServerCommand.php',
         'BeyondCode\\DumpServer\\DumpServerServiceProvider' => __DIR__ . '/..' . '/beyondcode/laravel-dump-server/src/DumpServerServiceProvider.php',
         'BeyondCode\\DumpServer\\Dumper' => __DIR__ . '/..' . '/beyondcode/laravel-dump-server/src/Dumper.php',
